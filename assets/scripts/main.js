946f13599fad04703cb318bda8d27b5d10e3f670
Reuben George <reuben_george1998@outlook.com>
2021-11-18 20:42:58 +0000

    function-restructuring

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,7 +9,7 @@ const alphabetInput = document.querySelector(".hangman__submit-guess__input");
 const inputSubmit = document.querySelector("#submit");
 
 
-let wordsArr = ["HTML", "CSS", "JavaScript", "flexbox", "grid", "bootstrap", "function", "responsive", "iterator", "object", "classes", "testing", "hangman"];
+const wordsArr = ["html", "css", "javascript", "flexbox", "grid", "bootstrap", "function", "responsive", "iterator", "object", "classes", "testing", "hangman"];
 
 let guess = "";
 let guessArr = [];
@@ -18,49 +18,20 @@ let word = wordsArr[Math.floor(Math.random()*wordsArr.length)];
 const wordArr = word.split("");
 
 
-const resetGame = () => {
-  guessNumber.innerHTML = `6`;
-  alphabets.forEach((alphabet) => {
-    alphabet.style.display = "flex";
-  });
-  letters.forEach((letter) => {
-    letter.style.display = "none";
-  });
-  guessArr = [];
-  correctGuessArr = [];
-  gameEndOutput.innerHTML = `<p class="hangman__word__counter__text">Guesses left:&nbsp; </p><p class="hangman__word__counter__text guess-number">6</p>`;
-}
-
+// generate the blank spaces where the words go
 const generateBlankSpaces = (index) => {
   return `<div class = "alphabet-guess" value = "${index}"> <h2 class = "letter"> ${wordArr[index]} </h2></div>`;
 }
 
-const generateWord = () => {
+// main function that sets up the board when the game is first loaded
+const gameInit = () => {
   for (let i = 0; i < wordArr.length; i++) {
     wordDisplay.innerHTML += generateBlankSpaces(i);
   }
 }
+gameInit();
 
-generateWord();
-
-
-const losingMessage = () => {
-  gameEndOutput.innerHTML = `<h2 class = "hangman__word__counter__winning-message"> BETTER LUCK NEXT TIME ._. </h2>`;
-}
-
-
-const guessCounterCountdown = () => {
-  let guessCount = parseInt(guessNumber.innerHTML);
-  guessCount -= 1;
-  if (guessCount > 0) {
-    guessNumber.innerHTML = `${guessCount}`;
-  }
-  else {
-    losingMessage();
-  }
-}
-
-
+// returns the indexes of the letter of the correct guess within the word array
 const getCorrectGuessIndex = (arr, val) => {
   const indexes = [];
   let i = -1;
@@ -73,61 +44,83 @@ const getCorrectGuessIndex = (arr, val) => {
 
 const letters = document.querySelectorAll(".letter");
 
-const makeDisplayFlex = () => {
+// displays the letter above a dash after it's been correctly guessed
+const showGuess = (guess, wordArr) => {
   const guessIndexArr = getCorrectGuessIndex(wordArr, guess);
   guessIndexArr.map((index) => {
     letters[index].style.display = "flex";
   });
 }
 
-
-const winningMessage = () => {
-  gameEndOutput.innerHTML = `<h2 class = "hangman__word__counter__winning-message"> WINNER WINNER CHICKEN DINNER </h2>`;
-}
-
-const removeAlphabet = () => {
-  alphabets.forEach((alphabet) => {
-    if (guess.toLowerCase() == alphabet.innerHTML) {
-        alphabet.style.display="none";
+// removing the letter from letter display if it's been guessed
+const removeLetter = (alphabet) => {
+  alphabet.forEach((letter) => {
+    if (guess == letter.innerHTML) {
+        letter.style.display="none";
     }
   });
 }
 
+// returns true or false based on whether it's a correctly guesssed letter
+const isCorrectGuess = (guess, word) => {
+  return word.includes(guess);
+}
 
-const checkAlphabet = () => {
-  const isCorrectGuess = word.toLowerCase().includes(guess.toLowerCase());
-  const isNotNewGuess = guessArr.includes(guess.toLowerCase());
-  console.log(isNotNewGuess);
-  if(!isNotNewGuess) {
-    if (isCorrectGuess) {
-      removeAlphabet();
-      const uniqWordArr = [...new Set(wordArr)];
-      guessArr.push(guess.toLowerCase());
-      correctGuessArr.push(guess.toLowerCase());
-      if (correctGuessArr.length == uniqWordArr.length) {
-        makeDisplayFlex();
-        winningMessage();
-      }
-      else {
-        makeDisplayFlex();
-      }
-     
-    }
-    else {
-      guessArr.push(guess.toLowerCase());
-      removeAlphabet();
-      guessCounterCountdown();
-    }
-  }
+// returns true if the guessArr
+const isNotNewGuess = (guess, guessArr) => {
+  return guessArr.includes(guess);
+}
+
+// returns true if the game has been won
+const hasWon = (guessArr, wordArr) => {
+  return guessArr.length == wordArr.length;
 }
 
 
 const handleClickGuess = () => {
-  guess = alphabetInput.value;
+  guess = alphabetInput.value.toLowerCase();
   alphabetInput.value = "";
-  checkAlphabet();
+  // 1. Remove the letter from the keyboard display
+  removeLetter(alphabets);
+
+  console.log(isNotNewGuess(guess, guessArr));
+  console.log(isCorrectGuess(guess, word));
+  console.log(isNotNewGuess(guess, guessArr) && isCorrectGuess(guess, word));
+  
+  if (!isNotNewGuess(guess, guessArr)){
+    // 2. Add the guess to the guess array of letters
+    guessArr.push(guess);
+    // 3. Is it a correct guess?
+    if(isCorrectGuess(guess, word)){
+      // Get unique letters from the word array, because a word can have the same letter multiple times
+      const uniqWordArr = [...new Set(wordArr)];
+      showGuess(guess, wordArr);
+      correctGuessArr.push(guess);
+      // Check if the game's been won
+      if (hasWon(uniqWordArr, correctGuessArr)) {
+        gameEndOutput.innerHTML = `<h2 class = "hangman__word__counter__winning-message"> WINNER WINNER CHICKEN DINNER </h2>`;
+      }
+    }
+  }
+  // 4. What happens if it is an incorrect guess?
+  else {
+    // Get and lower guess count
+    let guessCount = parseInt(guessNumber.innerHTML);
+    guessCount -= 1;
+    // If guess count is greater than 0, update the innerHTMl with guess count
+    if (guessCount > 0) {
+      guessNumber.innerHTML = `${guessCount}`;
+    } else { // If it's lower than 0, then display the 
+      gameEndOutput.innerHTML = `<h2 class = "hangman__word__counter__winning-message"> BETTER LUCK NEXT TIME ._. </h2>`;
+    }
+  }
+}
+
+// resets board by reloading page
+const handleClickReset = () => {
+  document.location.reload();
 }
 
 
 inputSubmit.addEventListener("click", handleClickGuess);
-document.querySelector("#reset").addEventListener("click", resetGame);
+document.querySelector("#reset").addEventListener("click", handleClickReset);

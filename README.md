d6bda806cd105776a657c3563069cb9873a048eb
Reuben George <reuben_george1998@outlook.com>
2021-11-20 15:56:46 +0000

    read-me-and-cleaner-code

---
+++ README.md
@@ -0,0 +1,50 @@
+# JavaScript Hangman
+### Author - Reuben George
+A chalkboard-based hangman game.
+
+
+## General Info
+This javascript game was made during the fourth week of my _nology course where we were taught more about functions and DOM manipulation along with testing, aspects of which I've implemented within my own code-writing.
+
+### Technologies used
+HTML, CSS, SCSS, JavaScript
+
+### Design Inspiration
+The inspiration for me was to have a clean blackboard design for hangman similar to emulate the more nostalgic classic game.
+
+
+## How it works
+
+The user inputs their guess using a keyboard/mobile keyboard and clicks sumbit. So first the guessed letter will be removed from the letters array below the guess word.
+
+Then based on their guess two actions occur; if it's a correct guess then the letter will be revealed within the word. If it's an incorrect guess, the guesses left number will decrement.
+
+The game is won when all the letters have been guessed correctly before the guesses left number reaches zero.
+
+The game is lost when the guesses left counter reaches zero before the word has been guessed.
+
+
+## The code
+
+### Helper functions
+- `setGuessCounter()`: sets the guessLeft to the counter innerHTML
+- `setBlankSpaces()`: sets the blank spaces to guess the word
+- `getCorrectGuessIndex()`: returns the indices of the correct guess within the word array
+- `showGuess()`: displays the letter above a dash after it's been correctly guessed
+- `removeLetter()`: removing the letter from the letter display if it's been guessed
+- `isCorrectGuess()`: returns true or false based on whether it's a correctly guessed letter
+- `isNotNewGuess()`: returns true if the guess is included within the total guesses array
+- `hasWon()`: returns true if the game has been won (based on whether the array with the correct guesses is the same length as the word array)
+- `setGameEndMsg()`: used to display a message at the end of the game, the message is customisable based on the parameter included
+- `decrementCount()`: returns guesses left - 1 unless the guesses left is zero, then it just returns the original input
+- `resetVariable()`: used to reset any variable based on their type
+
+### Main functions
+- `gameInit()`: function used to initialise the game and set the board
+- `handleClickGuess()`: function attached to the submitBtn used to enter the letter guess and display on the board based on whether it's a correct guess or not
+- `handleClickReset()`: function used to reset the board and game (not totally functioning yet as the guesses left doesn't display again once the board has been reset)
+
+
+
+## Future implementation
+As my reset button it's completely functioning just yet, I think the next step would be figuring out how to fix that so it has full functionality. With that being a smaller task, the next step would be for me to animate a hangman graphic which dynamically changes based on the number of guesses left.
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,33 +1,41 @@
-const wordDisplay = document.querySelector(".guess");
-const guessNumber = document.querySelector(".guess-number");
+const wordDisplay = document.querySelector(".countdown__guess-word");
+const guessNumber = document.querySelector(".countdown__guess-number");
 
-const alphabets = document.querySelectorAll(".alphabet");
+const alphabets = document.querySelectorAll(".guessing__letter");
 
-const gameEndOutput = document.querySelector(".hangman__word__counter");
+const gameEndOutput = document.querySelector(".countdown__container");
 
-const alphabetInput = document.querySelector(".hangman__submit-guess__input");
-const inputSubmit = document.querySelector("#submit");
+const alphabetInput = document.querySelector(".user-inputs__guess");
+const submitBtn = document.querySelector("#submit");
 
 
-const wordsArr = ["html", "css", "javascript", "flexbox", "grid", "bootstrap", "function", "responsive", "iterator", "object", "classes", "testing", "hangman"];
+const wordsArr = ["html", "css", "javascript", "flexbox", "grid", "bootstrap", "function", "responsive", "iterator", "object", "classes", "testing", "hangman", "specificity", "button", "main", "div"];
+
 
 let guess = "";
 let guessArr = [];
 let correctGuessArr = [];
-let word = wordsArr[Math.floor(Math.random()*wordsArr.length)];
-const wordArr = word.split("");
+let word = wordsArr[Math.floor(Math.random()*wordsArr.length)].toLowerCase();
+let wordArr = word.split("");
+let guessesLeft = 6;
 
+
+// set the guess counter number on the screen
+const setGuessCounter = (count) => {
+  guessNumber.innerHTML = `${count}`;
+}
 
 // generate the blank spaces where the words go
-const generateBlankSpaces = (index) => {
-  return `<div class = "alphabet-guess" value = "${index}"> <h2 class = "letter"> ${wordArr[index]} </h2></div>`;
+const setBlankSpaces = (index, wordArr) => {
+  return `<div class = "countdown__letter-div" value = "${index}"> <h2 class = "countdown__letter"> ${wordArr[index]} </h2></div>`;
 }
 
 // main function that sets up the board when the game is first loaded
 const gameInit = () => {
   for (let i = 0; i < wordArr.length; i++) {
-    wordDisplay.innerHTML += generateBlankSpaces(i);
+    wordDisplay.innerHTML += setBlankSpaces(i, wordArr);
   }
+  setGuessCounter(guessesLeft);
 }
 gameInit();
 
@@ -42,13 +50,12 @@ const getCorrectGuessIndex = (arr, val) => {
 }
 
 
-const letters = document.querySelectorAll(".letter");
+let letters = document.querySelectorAll(".countdown__letter");
 
 // displays the letter above a dash after it's been correctly guessed
-const showGuess = (guess, wordArr) => {
-  const guessIndexArr = getCorrectGuessIndex(wordArr, guess);
+const showGuess = (nodeList, guessIndexArr) => {
   guessIndexArr.map((index) => {
-    letters[index].style.display = "flex";
+    nodeList[index].style.display = "flex";
   });
 }
 
@@ -66,7 +73,7 @@ const isCorrectGuess = (guess, word) => {
   return word.includes(guess);
 }
 
-// returns true if the guessArr
+// returns true if the guess is included within the total guess array
 const isNotNewGuess = (guess, guessArr) => {
   return guessArr.includes(guess);
 }
@@ -76,51 +83,116 @@ const hasWon = (guessArr, wordArr) => {
   return guessArr.length == wordArr.length;
 }
 
+// sets the message at the end of the game
+const setGameEndMsg = (msg) => {
+  gameEndOutput.innerHTML = `<h2 class = "countdown__end-message"> ${msg} </h2>`;
+}
+
+// decrement the life counter
+const decrementCount = (count) => {
+  if (count > 0) {
+    const decremented = --count;
+    return decremented;
+  }
+  else {
+    return count;
+  }
+}
+
+// used to reset any variables based on their type
+const resetVariable = (variable) => {
+  if (typeof variable == "string") {
+    variable = "";
+    return variable;
+  }
+  else if (typeof variable == "object") {
+    variable = [];
+    return variable;
+  }  
+}
 
 const handleClickGuess = () => {
   guess = alphabetInput.value.toLowerCase();
+  // so that the input is ready for the next guess
   alphabetInput.value = "";
   // 1. Remove the letter from the keyboard display
   removeLetter(alphabets);
-
-  console.log(isNotNewGuess(guess, guessArr));
-  console.log(isCorrectGuess(guess, word));
-  console.log(isNotNewGuess(guess, guessArr) && isCorrectGuess(guess, word));
   
-  if (!isNotNewGuess(guess, guessArr)){
-    // 2. Add the guess to the guess array of letters
+   // 2. Add the guess to the guess array of letters if it's a new guess
+  if (!isNotNewGuess(guess, guessArr)) {
     guessArr.push(guess);
-    // 3. Is it a correct guess?
-    if(isCorrectGuess(guess, word)){
-      // Get unique letters from the word array, because a word can have the same letter multiple times
-      const uniqWordArr = [...new Set(wordArr)];
-      showGuess(guess, wordArr);
-      correctGuessArr.push(guess);
-      // Check if the game's been won
-      if (hasWon(uniqWordArr, correctGuessArr)) {
-        gameEndOutput.innerHTML = `<h2 class = "hangman__word__counter__winning-message"> WINNER WINNER CHICKEN DINNER </h2>`;
-      }
+  }
+
+  // 3. Is it a correct guess?
+  if (isCorrectGuess(guess, word)){
+    
+    // Get unique letters from the word array, because a word can have the same letter multiple times
+    const uniqWordArr = [...new Set(wordArr)];
+    // displays guess on the board
+    showGuess(letters, getCorrectGuessIndex(wordArr, guess));
+    correctGuessArr.push(guess);
+
+    // so the submit button can't be spam clicked
+    guess = resetVariable("string", guess);
+    
+    /* get unique letters of the correct guess as spamming submit would still cause game to be won if there isn't a unique array of correct guesses to check against */
+    const uniqCorrectGuessArr = [...new Set(correctGuessArr)];
+    // Check if the game's been won
+    // the .join("").split("") gets rid of any empty string entries
+    if (hasWon(uniqWordArr, uniqCorrectGuessArr.join("").split(""))) {
+       setGameEndMsg("WINNER WINNER CHICKEN DINNER");
+       submitBtn.disabled = true;
     }
   }
   // 4. What happens if it is an incorrect guess?
   else {
-    // Get and lower guess count
-    let guessCount = parseInt(guessNumber.innerHTML);
-    guessCount -= 1;
-    // If guess count is greater than 0, update the innerHTMl with guess count
-    if (guessCount > 0) {
-      guessNumber.innerHTML = `${guessCount}`;
-    } else { // If it's lower than 0, then display the 
-      gameEndOutput.innerHTML = `<h2 class = "hangman__word__counter__winning-message"> BETTER LUCK NEXT TIME ._. </h2>`;
+    // lowers the guesses left
+    guessesLeft = decrementCount(guessesLeft);
+    setGuessCounter(guessesLeft);
+    // if guessesLeft = 0, end the game
+    if (guessesLeft == 0) {
+      setGameEndMsg("BETTER LUCK NEXT TIME ._.");
+      letters.forEach((letter) => {
+        letter.style.display = "flex";
+      });
+      submitBtn.disabled = true;
     }
   }
 }
 
+
 // resets board by reloading page
 const handleClickReset = () => {
-  document.location.reload();
+  
+  guessesLeft = 6;
+  guess = resetVariable(guess);
+  guessArr = resetVariable(guessArr);
+  correctGuessArr = resetVariable(correctGuessArr);
+  
+  alphabets.forEach((alphabet) => {
+    alphabet.style.display = "flex";
+  });
+  letters.forEach((letter) => {
+    letter.style.display = "none";
+  });
+
+  // reset number doesn't show up when the game is reset due to this
+  gameEndOutput.innerHTML = `<p class="countdown__title-text">Guesses left:&nbsp; </p>
+  <p class="countdown__guess-number"></p>`;
+
+  word = wordsArr[Math.floor(Math.random()*wordsArr.length)].toLowerCase();
+  wordArr = word.split("");
+
+  wordDisplay.innerHTML = ``;
+  gameInit();
+  
+  letters = document.querySelectorAll(".countdown__letter");
+
+  
+  // document.location.reload();
 }
 
 
-inputSubmit.addEventListener("click", handleClickGuess);
+submitBtn.addEventListener("click", handleClickGuess);
 document.querySelector("#reset").addEventListener("click", handleClickReset);
+document.querySelector("#reset").addEventListener("click", setGuessCounter(guessesLeft));
